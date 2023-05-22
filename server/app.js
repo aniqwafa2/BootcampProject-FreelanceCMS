13508d4bf04c279684059f120921cfedf094fd85
arelhabib <arelhabib@gmail.com>
2023-05-22 11:35:49 +0700

    cms & server update: - cms build to server  & server can serve cms app

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const routes = require("./routes");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -16,22 +17,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
 // routes
-app.get("/api", (req, res) => {
-  res.json(
-    `welcome to CMS API, go to ${req.headers.host}/api/docs to open documentation`
-  );
-});
-
+app.use(express.static("dashboard"));
 app.use(express.static("public"));
 app.use("/api", routes);
 
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dashboard", "index.html"));
+});
+
 // NOTE: custom msg route notfound,
 // harus ditaruh dibawah route yg udah dibuat
-app.get("*", function (req, res) {
-  res.status(404).json({
-    message: "route not found",
-  });
-});
+// app.get("*", function (req, res) {
+//   res.status(404).json({
+//     message: "route not found",
+//   });
+// });
 
 app.listen(PORT, () => {
   console.log(`Server API is listening to port: ${PORT}`);
